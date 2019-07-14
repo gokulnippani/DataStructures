7f0efc5fd2e8ab8200690fdf83e224245222bbb5
gokulnippani <gokulnippani@gmail.com>
2019-07-14 00:40:44 -0500

    search in rotated array

--- Project_3/problem_2.py
+++ Project_3/problem_2.py
@@ -7,7 +7,27 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
+    return search(input_list,0,len(input_list)-1,number)
     pass
+
+
+def search(arr, l, h, key):
+    if l > h:
+        return -1
+
+    mid = (l + h) // 2
+    if arr[mid] == key:
+        return mid
+
+    if arr[l] <= arr[mid]:
+
+        if key >= arr[l] and key <= arr[mid]:
+            return search(arr, l, mid - 1, key)
+        return search(arr, mid + 1, h, key)
+
+    if key >= arr[mid] and key <= arr[h]:
+        return search(a, mid + 1, h, key)
+    return search(arr, l, mid - 1, key)
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
